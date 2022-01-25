129391248ca1e16aa5ec89ab01fab821256e9a1e
Sibarra96 <cheibarre03@gmail.com>
2022-01-25 17:44:13 -0600

    module 7.1

--- getting-started/course/home.js
+++ getting-started/course/home.js
@@ -1,5 +1,26 @@
-const  button = document.getElementById('see-review');
+/*
+const values_1 = ['a','b','c','d','e'];
+const values_2 = ['a','bbb','ccc','d','e'];
+const values_3 = ['a','b','c','d','e'];
 
+const usingFilter = values_1.filter(function(item){
+    return item > 'b';
+});
+
+const usingFind = values_2.find(function(item){
+    return item.length > 1;
+});
+
+console.log('Filter: ', usingFilter);
+console.log('Find: ', usingFind);
+
+const usingForEach = values_3.forEach(function(item){
+    return item;
+});
+*/
+
+/*
+const  button = document.getElementById('see-review');
 
 button.addEventListener('click',() =>{
     const review = document.getElementById('review'); 
@@ -10,6 +31,5 @@ button.addEventListener('click',() =>{
         review.classList.add('d-none');
         button.textContent = 'See Review';
     }
-});
-
- 
+}); 
+*/
